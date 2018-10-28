f1c59636c5f555c9da82ebe4a07dc03c58aae968
James Ray <james@rayprogramming.com>
2018-10-28 12:30:04 -0400

    Init commit

---
+++ src/main/java/com/rihoj/sortermod/Main.java
@@ -0,0 +1,10 @@
+package com.rihoj.sortermod;
+
+import com.rihoj.sortermod.util.Reference;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
+public class Main {
+	
+}
---
+++ src/main/java/com/rihoj/sortermod/util/Reference.java
@@ -0,0 +1,12 @@
+package com.rihoj.sortermod.util;
+
+public class Reference {
+
+	public static final String MOD_ID = "rsortm";
+	public static final String NAME = "Sorter Mod";
+	public static final String VERSION = "1.0";
+	public static final String ACCEPTED_VERSION = "[1.12.2]";
+	public static final String CLIENT_PROXY_CLASS = "";
+	public static final String COMMON_PROXY_CLASS = "";
+	
+}
